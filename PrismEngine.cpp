a91e9f2e40a302986fcaf59d7764afaa256b7dc0
Aleksei-Kutuzov <alekseikutuzov2@yandex.com>
2025-01-22 00:26:50 +0300

    поверхностная чистка

--- PrismEngine.cpp
+++ PrismEngine.cpp
@@ -5,214 +5,6 @@
 
 
 
-void main_() {
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-    SDL_Surface* surface;
-    SDL_Texture* texture;
-    int done;
-    SDL_Event event;
-
-
-    if (SDL_CreateWindowAndRenderer(800, 480, SDL_WINDOW_FULLSCREEN_DESKTOP, &window, &renderer) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-            "SDL_CreateWindowAndRenderer() failed: %s", SDL_GetError());
-        return;
-    }
-
-    if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG)
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "IMG_Init() failed :%s", SDL_GetError());
-        SDL_Quit();
-        return;
-    }
-
-    if (TTF_Init() < 0)
-    {
-        printf("Couldn't initialize SDL TTF: %s\n", SDL_GetError());
-        exit(1);
-    }
-
-    //PRISM_Render PrismRender(renderer);
-
-
-    SDL_DisplayMode mode;
-    SDL_GetDisplayMode(0, 0, &mode);
-    SDL_SetWindowSize(window, 480, 800);
-    AbstractCamera3D MainCamera(0.1, 1000.0f, 90.0f,
-        mode.w, mode.h);
-    MainCamera.SetPosition({ 0, 1, -30 }, { 0, 0, 0 }, { 0, 0, 1 });
-    struct PRISM_Vector3d vectcube = { 0, 0, 8 };
-    AbstractObject3D Obj1(vectcube, { 0, 0, 180 }, { 1, 1, 1 }, MainCamera);
-    AbstractObject3D Obj2({ 0, 0, 8 }, { 0, 0, 180 }, { 1, 1, 1 }, MainCamera);
-    PRISM_Mesh meshObj1;
-    meshObj1.tris = {
-        // SOUTH
-        { {0.0f, 0.0f, 0.0f},    {0.0f, 1.0f, 0.0f},    {1.0f, 1.0f, 0.0f} },
-        { {0.0f, 0.0f, 0.0f},    {1.0f, 1.0f, 0.0f},    {1.0f, 0.0f, 0.0f} },
-
-        // EAST
-        { {1.0f, 0.0f, 0.0f},    {1.0f, 1.0f, 0.0f},    {1.0f, 1.0f, 1.0f} },
-        { {1.0f, 0.0f, 0.0f},    {1.0f, 1.0f, 1.0f},    {1.0f, 0.0f, 1.0f} },
-
-        // NORTH
-        { {1.0f, 0.0f, 1.0f},    {1.0f, 1.0f, 1.0f},    {0.0f, 1.0f, 1.0f} },
-        { {1.0f, 0.0f, 1.0f},    {0.0f, 1.0f, 1.0f},    {0.0f, 0.0f, 1.0f} },
-
-        // WEST
-        { {0.0f, 0.0f, 1.0f},    {0.0f, 1.0f, 1.0f},    {0.0f, 1.0f, 0.0f} },
-        { {0.0f, 0.0f, 1.0f},    {0.0f, 1.0f, 0.0f},    {0.0f, 0.0f, 0.0f} },
-
-        // TOP
-        { {0.0f, 1.0f, 0.0f},    {0.0f, 1.0f, 1.0f},    {1.0f, 1.0f, 1.0f} },
-        { {0.0f, 1.0f, 0.0f},    {1.0f, 1.0f, 1.0f},    {1.0f, 1.0f, 0.0f} },
-
-        // BOTTOM
-        { {1.0f, 0.0f, 1.0f},    {0.0f, 0.0f, 1.0f},    {0.0f, 0.0f, 0.0f} },
-        { {1.0f, 0.0f, 1.0f},    {0.0f, 0.0f, 0.0f},    {1.0f, 0.0f, 0.0f} },
-    };
-    //    Obj1.SetMesh(meshObj1);
-    //    Obj2.DownloadFromOBJ("hause2.obj");
-    std::vector<Joystick> joysticks(2); // Для двух джойстиков
-    InitJoystick(joysticks[0], 300, mode.h - 300, 300, 150, renderer); // Инициализация первого джойстика
-    InitJoystick(joysticks[1], mode.w - 300, mode.h - 300, 300, 150, renderer); // Инициализация второго джойстика
-
-    //PrismRender.AddInRender(Obj1);
-    //PrismRender.AddInRender(Obj2);
-
-    ToggleButton DisplayTriangleContours;
-    ToggleButton DisplayDimming1;
-    ToggleButton DisplayDimming2;
-    ToggleButton Zbuffer;
-
-    InitToggleButton(DisplayDimming1, 650, 50, 370, 80, true, renderer);
-    InitToggleButton(DisplayDimming2, 1040, 50, 370, 80, false, renderer);
-    InitToggleButton(DisplayTriangleContours, 50, 50, 580, 80, false, renderer);
-    InitToggleButton(Zbuffer, 1435, 50, 260, 80, true, renderer);
-
-    SetToggleButtonText(DisplayDimming1, "DisplayDimming1",
-        "fonts/Bartina Semibold.ttf", { 155, 155, 1, 255 }, 64);
-    SetToggleButtonText(DisplayDimming2, "DisplayDimming2",
-        "fonts/Bartina Semibold.ttf", { 155, 155, 1, 255 }, 64);
-    SetToggleButtonText(DisplayTriangleContours, "DisplayTriangleContours",
-        "fonts/Bartina Semibold.ttf", { 155, 155, 1, 255 }, 64);
-    SetToggleButtonText(Zbuffer, "Zbuffer",
-        "fonts/Bartina Semibold.ttf", { 155, 155, 1, 255 }, 64);
-
-    int ftimeline = 1;
-    done = 0;
-    while (!done) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
-            {
-                done = 1;
-            }
-            //			HandleInput(event, joysticks, mode.w, mode.h); // Обработка событий для всех джойстиков
-            ToggleButtonInput(DisplayDimming1, event);
-            ToggleButtonInput(DisplayDimming2, event);
-            ToggleButtonInput(DisplayTriangleContours, event);
-            ToggleButtonInput(Zbuffer, event);
-
-            const Uint8* state = SDL_GetKeyboardState(NULL);
-
-
-            if (state[SDL_SCANCODE_ESCAPE]) {
-                ;
-            }
-
-            if (state[SDL_SCANCODE_UP]) {
-                MainCamera.Translate.z += 1;
-            }
-
-            if (state[SDL_SCANCODE_DOWN]) {
-                MainCamera.Translate.z -= 1;
-            }
-
-            if (state[SDL_SCANCODE_LEFT]) {
-                MainCamera.Translate.x -= 1;
-            }
-
-            if (state[SDL_SCANCODE_RIGHT]) {
-                MainCamera.Translate.x += 1;
-            }
-
-            if (state[SDL_SCANCODE_W]) {
-                MainCamera.Translate.y -= 1;
-            }
-
-            if (state[SDL_SCANCODE_S]) {
-                MainCamera.Translate.y += 1;
-            }
-
-            if (state[SDL_SCANCODE_A]) {
-                MainCamera.Rotation.y += 0.1;
-            }
-
-            if (state[SDL_SCANCODE_D]) {
-                MainCamera.Rotation.y -= 0.1;
-            }
-
-            if (state[SDL_SCANCODE_4]) {
-                Obj1.Rotation.z -= 1;
-            }
-
-            if (state[SDL_SCANCODE_6]) {
-                Obj1.Rotation.z += 1;
-            }
-
-            if (state[SDL_SCANCODE_8]) {
-                Obj1.Rotation.y += 1;
-            }
-
-            if (state[SDL_SCANCODE_2]) {
-                Obj1.Rotation.y += 1;
-            }
-            Obj1.SetRotateXYZ(Obj1.Rotation);
-            MainCamera.SetPosition(MainCamera.Translate, MainCamera.Rotation, { 0, 0, 1 });
-
-
-        }
-        clock_t start = clock();
-        //        ftimeline ++;
-        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-        SDL_RenderClear(renderer);
-        //		Obj1.Rotation.y += {0, 1, 0};
-        //		Obj1.Rotation.z += {0, 0, 0};
-        //        Obj1.SetRotateXYZ(Obj1.Rotation);
-
-
-        //		MainCamera.SetPosition(vectrotcube, {0, 0, 0}, {0, 0, 1});
-
-        //		Obj2.Rotation.y -= vectrotcube.y;
-        //		Obj2.Rotation.z -= vectrotcube.z;
-        //        Obj2.SetRotateXYZ(Obj2.Rotation);
-
-        //		Normal - {t,f,f,t,f,t,5}
-        //        Obj1.DrawMeshTriangles(renderer, {DisplayDimming1.active, DisplayDimming2.active, DisplayTriangleContours.active, Zbuffer.active, false, true, 5});
-        //		Obj2.DrawMeshTriangles(renderer, {DisplayDimming1.active, DisplayDimming2.active, DisplayTriangleContours.active, Zbuffer.active, false, true, 5});
-        //
-        //PrismRender.RenderFrameSurfase(mode.w, mode.h, { DisplayDimming1.active, DisplayDimming2.active, DisplayTriangleContours.active, Zbuffer.active, false, true, 5 });
-
-
-        RenderToggleButton(DisplayDimming1);
-        RenderToggleButton(DisplayDimming2);
-        RenderToggleButton(DisplayTriangleContours);
-        RenderToggleButton(Zbuffer);
-
-
-        MainCamera.ClearZ_Buffer(renderer);
-        //			SDL_RenderPresent(renderer);
-
-        SDL_Delay(10);
-
-        clock_t end = clock();
-        float seconds = (float)(end - start) / CLOCKS_PER_SEC * 1000;
-        SDL_Log("Seconds RAN FUNK time ran ms: %f", seconds);
-    }
-
-    SDL_Quit();
-}
-
 
 class AbstractObject3D_demo {
 private:
@@ -403,7 +195,7 @@ int main(int argc, char* argv[])
     c.PrintInfo();
 
     // Создаем окно
-    PRISM_Window window(600, 400, u8"♦PRISME♦");
+    PRISM_Window window(600, 400, u8"♦PRISME♦|IT IS TEST");
 
     // Проверяем, удалось ли создать окно
     if (!window.isRunning()) {
@@ -417,13 +209,11 @@ int main(int argc, char* argv[])
     
     AbstractObject3D object{ {0,0,0},{0,0,0},{1,1,1},MainCamera };
 
-    object.SetMesh(MeshLoader::LoadOBJ("Tesla Laser.obj", MainCamera)); 
-    object.SetMesh(MeshLoader::LoadOBJ("planet.obj", MainCamera));
+    object.SetMesh(MeshLoader::LoadOBJ("test_res/objs/Tesla Laser.obj", MainCamera));
+    object.SetMesh(MeshLoader::LoadOBJ("test_res/objs/planet.obj", MainCamera));
     
     //object.mesh.print();
 
-    object.materialAmbient = { 0.247f, 0.199f, 0.074f }; //золото
-    object.kSpecular = 0.1f;
     scene.AddObject(&object);
     scene.SetCamera(&MainCamera);
 
